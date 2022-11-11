332dcef5a7563471439a049b575dffef7deb9c5c
narva <narva@DESKTOP-VBQOST8>
2022-11-11 11:46:49 +0530

    commit

---
+++ src/test/java/demoProject/demoTest.java
@@ -0,0 +1,19 @@
+package demoProject;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	
+	
+	@Test
+	public void RegrationTest()
+	{
+		System.out.println("hellow world");
+	}
+
+	@Test
+	public void Smoke()
+	{
+		System.out.println("Hin world");
+	}
+}
